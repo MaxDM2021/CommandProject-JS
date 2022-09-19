1faaaa11695a3a87f579443df979d9878f6b54a5
MaxDM2021 <95755128+MaxDM2021@users.noreply.github.com>
2022-09-19 10:24:48 +0000

    Deploying to gh-pages from @ MaxDM2021/CommandProject-JS@0d249a4707c58f28a518c90a3f188497aab1f71e 🚀

---
+++ my-library.4db08fe0.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-closes]"),modal:document.querySelector("[data-modal]")};e.openModalBtn.addEventListener("click",(function(){e.modal.classList.remove("is-hidden")})),e.closeModalBtn.addEventListener("click",(function(){e.modal.classList.add("is-hidden")}))})();
+//# sourceMappingURL=my-library.4db08fe0.js.map
