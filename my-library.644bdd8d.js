14fff719d522b1e6e2dc85b124baaac177da68e3
MaxDM2021 <95755128+MaxDM2021@users.noreply.github.com>
2022-09-19 13:25:48 +0000

    Deploying to gh-pages from @ MaxDM2021/CommandProject-JS@50115bfa5f433d3e2220368614eeb8017a1b33f0 🚀

--- my-library.644bdd8d.js
+++
@@ -1,2 +0,0 @@
-!function(){var e;(e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-closes]"),modal:document.querySelector("[data-modal]")}).openModalBtn.addEventListener("click",(function(){e.modal.classList.remove("is-hidden")})),e.closeModalBtn.addEventListener("click",(function(){e.modal.classList.add("is-hidden")}))}();
-//# sourceMappingURL=my-library.644bdd8d.js.map
